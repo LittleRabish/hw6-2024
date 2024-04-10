f31c95522063e575e66db4c9f75a715bc07e7a17
LittleRabish <muyanh@umich.edu>
2024-04-10 01:55:50 -0400

    First try

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# hw7-2022
-Starter Code for HW7 - JavaScript basics with Video
+# SI 539 - HW7: JavaScript basics with Video
+
+[Specifications](https://docs.google.com/document/d/1--LXEcjTnpgSIevmX4vTuWC0lsr8HuP-bPZA48nQ_n8/edit?usp=sharing)
--- js/video.js
+++ js/video.js
@@ -1,11 +1,58 @@
 var video;
 
 window.addEventListener("load", function() {
-	console.log("Good job opening the window")
-
+    video = document.querySelector("#player1");
+    video.autoplay = false;
+    video.loop = false;
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+document.querySelector("#play").addEventListener("click", function() {
+	// console.log("Play Video");
+    video.play();
+});
 
+document.querySelector("#pause").addEventListener("click", function() {
+    video.pause();
+})
+
+document.querySelector("#slower").addEventListener("click", function() {
+    video.playbackRate = video.playbackRate * 0.9;
+	console.log("Speed is", video.playbackRate);
+})
+
+document.querySelector("#faster").addEventListener("click", function() {
+    video.playbackRate = video.playbackRate / 0.9;
+	console.log("Speed is", video.playbackRate);
+})
+
+document.querySelector("#skip").addEventListener("click", function() {
+    video.currentTime += 10;
+    // if (video.currentTime === video.duration) {
+    //     video.currentTime = 0;
+    // }
+    console.log("Video current time is", video.currentTime);
+})
+
+document.querySelector("#mute").addEventListener("click", function() {
+    if (video.muted === true) {
+        video.muted = false;
+        this.innerHTML = "Mute";
+    }
+    else {
+        video.muted = true;
+        this.innerHTML = "Unmute";
+    }
+})
+
+document.querySelector("#slider").addEventListener("input", function() {
+    video.volume = this.value / 100;
+    document.getElementById("volume").innerHTML = this.value + "%";
+})
+
+document.querySelector("#vintage").addEventListener("click", function() {
+    video.classList.add("oldSchool");
+})
+
+document.querySelector("#orig").addEventListener("click", function() {
+    video.classList.remove("oldSchool");
+})
